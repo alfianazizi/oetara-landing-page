ec7b140fd28c13e76410a9c24ba2c2e20bd7f066
desistmrym <desisitimaryam@gmail.com>
2024-11-28 09:53:11 +0700

    add headers

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,10 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import './App.css';
+import Headers from './layouts/Headers';
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <div>
-      <h1>Hello World</h1>
+      <Headers />
     </div>
   )
 }
---
+++ src/layouts/Headers.jsx
@@ -0,0 +1,29 @@
+import logo from '../assets/logo/logo.svg';
+import { FaUserLarge } from 'react-icons/fa6';
+import { IoSearchSharp } from 'react-icons/io5';
+import { RiMenu2Line } from 'react-icons/ri';
+
+const Headers = () => {
+  return (
+    <div className="fixed top-0 left-0 flex justify-between items-center py-6 px-5 md:px-10 w-full">
+        <div className="flex items-center gap-5 md:gap-10">
+            <img src={logo} alt="logo" className="w-8 h-8 md:w-10 md:h-10" />
+            <div className="view-desktop items-center gap-10">
+                <a href="#">Navigator</a>
+                <a href="#">Email</a>
+                <a href="#">Service</a>
+                <a href="#">Work</a>
+            </div>
+            <div className="view-mobile">
+                <RiMenu2Line className="text-2xl" />
+            </div>
+        </div>
+        <div className="flex items-center gap-8">
+            <IoSearchSharp className="text-2xl" />
+            <FaUserLarge className="text-2xl" />
+        </div>
+    </div>
+  )
+}
+
+export default Headers
